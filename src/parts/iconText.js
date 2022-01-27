e4f3f51063bd543a711f88e8369ae1c918c2dbce
handa26 <anandamuhammadmtq@gmail.com>
2022-01-27 08:41:58 +0700

    Create logo text

---
+++ src/parts/iconText.js
@@ -0,0 +1,11 @@
+import Button from "elements/Button";
+
+const iconText = () => {
+	return (
+		<Button className="brand-text-icon" href="" type="link">
+			Stay<span className="text-gray-900">cation.</span>
+		</Button>
+	);
+};
+
+export default iconText;
